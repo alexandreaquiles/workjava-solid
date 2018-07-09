592fc9e6c5968eacb8010b15523afd2f35e910b2
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-07-09 13:29:23 -0300

    cria classe TemaWorkJava que implementa Plugin

---
+++ code/tema-workjava/src/main/java/br/com/alexandreaquiles/workjava/TemaWorkJava.java
@@ -0,0 +1,19 @@
+package br.com.alexandreaquiles.workjava;
+
+import cotuba.domain.Capitulo;
+import cotuba.domain.Livro;
+import cotuba.plugin.Plugin;
+
+public class TemaWorkJava implements Plugin {
+
+	@Override
+	public void aposRenderizacaoMD(Capitulo capitulo) {
+
+	}
+
+	@Override
+	public void aoFinalizarGeracao(Livro livro) {
+
+	}
+
+}
