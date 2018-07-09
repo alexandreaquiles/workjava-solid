04d4dbcdbfe58ddc303bc555c19a11b8d5200cb0
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-07-09 15:54:18 -0300

    implementa contagem de palavras com um HashMap modificado, que acumula contagem de palavras adicionadas

--- code/cotuba-cli/src/main/java/cotuba/estatisticas/CalculadoraEstatisticasLivro.java
+++ code/cotuba-cli/src/main/java/cotuba/estatisticas/CalculadoraEstatisticasLivro.java
@@ -1,21 +1,20 @@
 package cotuba.estatisticas;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import cotuba.domain.Capitulo;
 import cotuba.domain.Livro;
 
 public class CalculadoraEstatisticasLivro {
 
-	public void contaPalavras(Livro livro) {
+	public ContagemPalavras contaPalavras(Livro livro) {
 
-		Map<String, Integer> contagem = new HashMap<>();
+		ContagemPalavras contagem = new ContagemPalavras();
 		
 		for (Capitulo capitulo : livro.getCapitulos()) {
 			String conteudoHTML = capitulo.getConteudoHTML();
-			contagem.put(conteudoHTML, 1); //está certo???
+			contagem.adicionaPalavra(conteudoHTML);
 		}
+		
+		return contagem;
 	}
 
 }
---
+++ code/cotuba-cli/src/main/java/cotuba/estatisticas/ContagemPalavras.java
@@ -0,0 +1,20 @@
+package cotuba.estatisticas;
+
+import java.util.HashMap;
+
+public class ContagemPalavras extends HashMap<String, Integer> {
+
+	private static final long serialVersionUID = 1L;
+
+	public void adicionaPalavra(String palavra) {
+		Integer contagem = get(palavra);
+		
+		if (contagem != null) {
+			contagem++;
+		} else {
+			contagem = 1;
+		}
+		
+		put(palavra, contagem);
+	}
+}
