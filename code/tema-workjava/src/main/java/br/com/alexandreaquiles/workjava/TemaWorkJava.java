303cf830b6c813e800c67ff1eab61e7ce6ecce6f
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-07-09 14:45:29 -0300

    move CSS do tema WorkJava para arquivo próprio

--- code/tema-workjava/src/main/java/br/com/alexandreaquiles/workjava/TemaWorkJava.java
+++ code/tema-workjava/src/main/java/br/com/alexandreaquiles/workjava/TemaWorkJava.java
@@ -1,5 +1,9 @@
 package br.com.alexandreaquiles.workjava;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -12,12 +16,21 @@ public class TemaWorkJava implements Plugin {
 	@Override
 	public void aposRenderizacaoMD(Capitulo capitulo) {
 		System.out.println("Executando tema WorkJava para o capítulo: " + capitulo.getTitulo());
+
+		try {
+			String html = capitulo.getConteudoHTML();
+			Document document = Jsoup.parse(html);
+
+			Path cssPath = Paths.get(TemaWorkJava.class.getResource("/tema.css").toURI());
+			String css = new String(Files.readAllBytes(cssPath));
+
+			document.select("head").append("<style> " + css + " </style>");
+			
+			capitulo.setConteudoHTML(document.html());
+		} catch (Exception e) {
+			throw new RuntimeException("Erro ao ler CSS do tema WorkJava", e);
+		}
 		
-		String html = capitulo.getConteudoHTML();
-		Document document = Jsoup.parse(html);
-		document.select("head").append("<style>h1 { border: 1px dashed black; }</style>");
-		
-		capitulo.setConteudoHTML(document.html());
 	}
 
 	@Override
