e26f93e852df45259eff32eb41466c677204d633
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-07-09 16:10:01 -0300

    corrige contagem de palavras usando JSoup

--- code/cotuba-cli/src/main/java/cotuba/estatisticas/CalculadoraEstatisticasLivro.java
+++ code/cotuba-cli/src/main/java/cotuba/estatisticas/CalculadoraEstatisticasLivro.java
@@ -1,5 +1,8 @@
 package cotuba.estatisticas;
 
+import com.itextpdf.html2pdf.jsoup.Jsoup;
+import com.itextpdf.html2pdf.jsoup.nodes.Document;
+
 import cotuba.domain.Capitulo;
 import cotuba.domain.Livro;
 
@@ -11,7 +14,11 @@ public class CalculadoraEstatisticasLivro {
 		
 		for (Capitulo capitulo : livro.getCapitulos()) {
 			String conteudoHTML = capitulo.getConteudoHTML();
-			contagem.adicionaPalavra(conteudoHTML);
+			Document document = Jsoup.parse(conteudoHTML);
+			String textoCapitulo = document.body().text();
+			for (String palavra : textoCapitulo.split("\\s+")) {
+				contagem.adicionaPalavra(palavra);
+			}
 		}
 		
 		return contagem;
