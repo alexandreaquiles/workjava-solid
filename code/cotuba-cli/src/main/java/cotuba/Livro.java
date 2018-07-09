4f5726aab507564c0fc5c9ae59396ff98cb17e87
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-07-09 11:40:13 -0300

    adiciona domain model: classes Livro e Capitulo [erro de compilação]

---
+++ code/cotuba-cli/src/main/java/cotuba/Livro.java
@@ -0,0 +1,36 @@
+package cotuba;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Livro {
+	
+	private String formato;
+	
+	private Path arquivoDeSaida;
+	
+	private List<Capitulo> capitulos = new ArrayList<>();
+
+	public Livro(String formato, Path arquivoDeSaida) {
+		this.formato = formato;
+		this.arquivoDeSaida = arquivoDeSaida;
+	}
+
+	public String getFormato() {
+		return formato;
+	}
+
+	public Path getArquivoDeSaida() {
+		return arquivoDeSaida;
+	}
+
+	public List<Capitulo> getCapitulos() {
+		return capitulos;
+	}
+	
+	public void adicionaCapitulos(List<Capitulo> capitulos) {
+		this.capitulos.addAll(capitulos);
+	}
+
+}
