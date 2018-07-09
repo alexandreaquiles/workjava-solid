6b4af4a8f651d5470b94bfcb11b4510ab1ff7252
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-07-09 13:34:06 -0300

    adiciona CSS no capítulo em tema-workjava

--- code/tema-workjava/src/main/java/br/com/alexandreaquiles/workjava/TemaWorkJava.java
+++ code/tema-workjava/src/main/java/br/com/alexandreaquiles/workjava/TemaWorkJava.java
@@ -1,5 +1,8 @@
 package br.com.alexandreaquiles.workjava;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import cotuba.domain.Capitulo;
 import cotuba.domain.Livro;
 import cotuba.plugin.Plugin;
@@ -8,7 +11,13 @@ public class TemaWorkJava implements Plugin {
 
 	@Override
 	public void aposRenderizacaoMD(Capitulo capitulo) {
-
+		System.out.println("Executando tema WorkJava para o capítulo: " + capitulo.getTitulo());
+		
+		String html = capitulo.getConteudoHTML();
+		Document document = Jsoup.parse(html);
+		document.select("head").append("<style>h1 { border: 1px dashed black; }</style>");
+		
+		capitulo.setConteudoHTML(document.html());
 	}
 
 	@Override
